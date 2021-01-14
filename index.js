e608cb84995a93e0db90fefd4c4aaa0eee0e48ef
Hoang Van Triu <58316994+triuhv@users.noreply.github.com>
2021-01-14 14:10:44 +0700

    apply to Travis CI

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const http = require('http');
-
-const port = 8080;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-});
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello NodeJs!\n')
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
